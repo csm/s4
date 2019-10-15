ac39ba8f63659c52577e4646c71457cbae0e0f37
Casey Marshall <csm@noonhome.com>
2019-10-15 13:47:47 -0700

    Update to konserve 0.5.1.
    Fixes using file store for S4 storage.

--- project.clj
+++ project.clj
@@ -5,10 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [aleph "0.4.6"]
-                 ; if you want to use filestore, look here: https://github.com/replikativ/konserve/pull/27
-                 ; memory store works fine with 0.5.0
-                 ;[io.replikativ/konserve "0.6.0-SNAPSHOT"]
-                 [io.replikativ/konserve "0.5.0"]
+                 [io.replikativ/konserve "0.5.1"]
                  [io.replikativ/superv.async "0.2.9"]
                  [com.arohner/uri "0.1.2"]
                  [org.clojure/data.xml "0.2.0-alpha6"]]
